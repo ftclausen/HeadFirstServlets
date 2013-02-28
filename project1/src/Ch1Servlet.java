3348910d511c189bd2476d5dd0a45052a83d9a32
Friedrich Clausen <friedrich.clausen@blackboard.com>
2013-02-28 14:40:55 +0100

    Wrote my very first servlet!!!

---
+++ project1/src/Ch1Servlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Ch1Servlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                    HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        java.util.Date today = new java.util.Date();
+        out.println("<html> " +
+                    "<body>" +
+                    "<h1 align=center>Head first Chapter 1 servlet</h1>" +
+                    "<br>" + today + "</body>" + "</html>");
+        }
+}
