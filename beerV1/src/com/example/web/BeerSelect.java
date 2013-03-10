ebadb06438f09fe9ad4a3b9a4c5cadd35d938a17
Fred Clausen <fred@derf.nl>
2013-03-10 22:26:55 +0100

    V2 works now

--- beerV1/src/com/example/web/BeerSelect.java
+++ beerV1/src/com/example/web/BeerSelect.java
@@ -3,6 +3,8 @@ package com.example.web;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.util.*;
+import com.example.model.*;
 
 public class BeerSelect extends HttpServlet {
     public void doPost(HttpServletRequest request,
@@ -10,8 +12,14 @@ public class BeerSelect extends HttpServlet {
                     throws IOException, ServletException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("Beer Selection Advice<br>");
+        out.println("Beer Selection Advice<br><br>");
         String c = request.getParameter("color");
-        out.println("<br>Got beer color " + c);
+        
+        BeerExpert beerTips = new BeerExpert();
+        List<String> brands = beerTips.getBrands(c);
+
+        for (String s: brands) {
+            out.println("* " + s + "<br>");
+        }
     }
 }
