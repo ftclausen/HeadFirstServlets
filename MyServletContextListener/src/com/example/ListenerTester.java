7e1f762e46205e0b2877edb87dfbdc6943d78ee9
Fred Clausen <fred@derf.nl>
2013-04-15 22:43:22 +0200

    Listener and session experiments

--- MyServletContextListener/src/com/example/ListenerTester.java
+++ MyServletContextListener/src/com/example/ListenerTester.java
@@ -10,6 +10,31 @@ public class ListenerTester extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
     
+        HttpSession session = request.getSession();
+        session.setAttribute("foo", "42");
+        // session.setMaxInactiveInterval(3600);
+
+        if (session.isNew()) {
+            out.println("<h3>This is a new session (or session expired). Welcome new user</h3>");
+        } else {
+            out.println("<h3>Welcome back!</h3>");
+        }
+
+        long sessionLastAccessed = session.getLastAccessedTime();
+        long sessionMaxInactive = session.getMaxInactiveInterval()*1000;
+        long sessionCreated = session.getCreationTime();
+        long currentTime = System.currentTimeMillis();
+
+        out.println("Sesssion last accessed = " + sessionLastAccessed + "<br>");
+        out.println("Sesssion maxInactive = " + sessionMaxInactive + "<br>");
+        int hardTimeout = 10;
+        long remaining = (hardTimeout * 1000) - (currentTime - sessionCreated);
+        out.println("Remaining time to hard expiration = " + remaining / 1000);
+        if (remaining <= 0) {
+            session.invalidate();
+        }
+
+        out.println("<hr>");
         out.println("Testing context attributes set by listener. Right now. Right here<br>");
 
         Dog dog = (Dog) getServletContext().getAttribute("dog");
