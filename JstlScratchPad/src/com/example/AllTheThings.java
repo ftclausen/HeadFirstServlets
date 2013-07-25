a2316b55dea1f9ae35947ca32cc049d226f00aa3
Friedrich Clausen <friedrich.clausen@blackboard.com>
2013-07-25 17:26:53 +0200

    More JSTL stuff

--- JstlScratchPad/src/com/example/AllTheThings.java
+++ JstlScratchPad/src/com/example/AllTheThings.java
@@ -3,16 +3,35 @@ package com.example;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.util.*;
 
 public class AllTheThings extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
                     throws IOException, ServletException {
         String tip = request.getParameter("tip");
-        if (tip.equalsIgnoreCase("")) {
+        String incomingMovies = request.getParameter("movies");
+        if (tip == null || tip.equalsIgnoreCase("")) {
             System.out.println("WARN: Setting tip to standard value, none given.");
             tip = "No Tip <b>given!!</b>";
-        } 
+        }
+
+        List<String> movies = null;
+        if (incomingMovies == null || incomingMovies.equalsIgnoreCase("")) {
+            System.out.println("WARN: Setting movies to standard value, none given.");
+            movies = new ArrayList<String>();
+            movies.add("Default Dave Does Dallas");
+        } else {
+            movies = Arrays.asList(incomingMovies.split(","));
+        }
+        String food = request.getParameter("food");
+        if (request.getParameter("food") == null || 
+            request.getParameter("food").equalsIgnoreCase("")) {
+            food = "default dessert";
+        }
+            
+        request.setAttribute("movies", movies);
         request.setAttribute("tip", tip);
+        request.setAttribute("food", food);
         RequestDispatcher view = request.getRequestDispatcher("ShowAllTheThings.jsp");
         view.forward(request, response);
     }
--- JstlScratchPad/web/ShowAllTheThings.jsp
+++ JstlScratchPad/web/ShowAllTheThings.jsp
@@ -1,3 +1,4 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
@@ -7,7 +8,27 @@
 <body>
 <h1> Welcome to JSTL Scratch pad dude! </h1>
 <h3> These random things really tie the room together </h3>
-Today's Tip : <c:out value='${tip}' escapeXml='true'/> <br>
+Today's Tip : <c:out value='${tip}' escapeXml='true' default='No tip received!'/> <br>
+Movie List! 
+<table>
+    <c:forEach var="movie" items="${movies}" varStatus="iteratorCount" >
+        <tr>
+            <td><b>${iteratorCount["count"]})</b> ${movie}</td>
+        </tr>
+    </c:forEach>
+</table>
+
+<p>you chose food ${food}</p>
+
+<c:choose>
+    <c:when test="${food == 'carrot'}">
+        <h3>Carrots are healthy!</h3>
+    </c:when>
+    <c:otherwise>
+        <h3>Don't eat too much of ${food}!!</h3>
+    </c:otherwise>
+</c:choose>
+
 </body>
 </html>
 
